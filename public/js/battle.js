28d34bbf54d2ebdb69e80e8f9e235bf8fa996beb
Kate Foust <kate.e.foust@gmail.com>
2020-03-24 09:02:58 -0700

    fixed async error with random emeny generation

--- public/js/battle.js
+++ public/js/battle.js
@@ -8,11 +8,29 @@ var $spellDefendBtn = $("#spellDefend");
 var $spellHealBtn = $("#spellHeal");
 
 //this is where the spells and fighting appear
-var $battleEventsDiv = $("#battleEvents")
+var $battleEventsDiv = $("#battleEvents") 
+var $playerNameDiv= $("#yourName");
+var $enemyNameDiv = $("#enemyName");
 
-window.onload = function() {
-  this.$battleEventsDiv.text("you loaded me")
-};
+var heal=2;
+var defense=0;
+var deathEaterArray=[];
+
+var enemy;
+
+
+function startGame()
+{
+
+enemy = getOpponent(deathEaterArray);  
+// window.onload = function() {
+  this.$playerNameDiv.text(test.name)
+  this.$enemyNameDiv.text(enemy.name);
+  $("#enemyHP").text(" " + enemy.hitpoints);
+
+  
+
+// };
 //green button (easy)
 $spellOneBtn.on("click", function() {
   Combat.playerAttackEasy();
@@ -37,7 +55,11 @@ $spellHealBtn.on("click", function(){
   Combat.playerHeal();
 });
 
-heal=2;
+
+};
+
+
+
 
 // $leftArrow.on("click", function() {
 //   alert("Go to the room at the left!");
@@ -84,6 +106,9 @@ function getSpells(){
 
             //console.log(spellsArray)
 
+            getDeathEaters();
+
+
         });
 
 
@@ -91,7 +116,68 @@ function getSpells(){
 
 };
 
-//player object (temporary);
+
+
+//call spell function
+getSpells();
+
+
+var DeathEater = function (id, name, intelligence, hitpoints) {
+  this.id=id
+  this.name = name;
+ 
+  this.intelligence = intelligence;
+  this.hitpoints = hitpoints;
+  
+  this.printTest = function () {
+      console.log("Name: " + this.name);
+      console.log("\n-------------\n");
+
+      
+  };
+
+
+
+
+
+};
+
+function getDeathEaters() {
+  $.ajax(
+      {
+          url: queryURL + "characters" + "?key=" + key,
+          method: "GET"
+      }).then(function (response) {
+
+
+          console.log("==========================")
+          console.log("combat characters (malevolent)")
+          //these characters are all death eaters
+          console.log("==========================")
+
+
+          for (var i = 0; i < response.length; i++) {
+              if (response[i].deathEater === true) {
+                 // deathEaterArray.push({ name: response[i].name, });
+                 // use random
+                  deathEaterArray.push(new DeathEater(i, response[i].name, 5, 100+i))
+              };
+
+                             
+          };
+          
+          console.log(deathEaterArray);
+          //console.log(enemy);
+
+          startGame();
+      });
+
+
+  };
+
+
+
+  //player object (temporary);
 var Player = function (id, name, intelligence, hitpoints, defense, experience, house, location, agility, history) {
   this.id=id;
   this.name = name;
@@ -108,8 +194,6 @@ var Player = function (id, name, intelligence, hitpoints, defense, experience, h
 //test player
 var test = new Player (1, "kate", 5, 100, 0, 2, "Hufflepuff", 0, 120, []);
 
-//call spell function
-getSpells();
 
 //object holding all combat moves
 var Combat={
@@ -119,9 +203,9 @@ var Combat={
       
       //calculating hit- total damage == player's intelligence
       var totalDamage=test.intelligence;
-      deathEaterArray[0].hitpoints=deathEaterArray[0].hitpoints-totalDamage;
+      enemy.hitpoints=enemy.hitpoints-totalDamage;
       //update enemy hp
-      $("#enemyHP").text(" " + deathEaterArray[0].hitpoints);
+      $("#enemyHP").text(" " + enemy.hitpoints);
       
       //checks if enemy has lost all hitpoints; if yes, player wins
       if (deathEaterArray[0].hitpoints <= 0){
@@ -309,48 +393,10 @@ var Combat={
 
         $("#resultModalBody").append("You Lose!");
         $('#myModal').modal('toggle');
-          };        // for (var i=0; i<spellsArray.length;i++){
-      //     if (spellsArray[i].chtype==="opponent" && spellsArray[i].difficulty==="easy"){
-      //         console.log(spellsArray[i]);
-      //     }
-      // }
+          };        
   },
 
-  // opponentAttackEasy: function (){
-  //     var totalDamage= deathEaterArray[0].intelligence;
-  //     test.hitpoints=test.hitpoints-totalDamage;
-  //     console.log("test's hp:" + test.hitpoints);
-  //     var spell=spellsArray[2];
-  //     console.log(spell);
-
-
-  // },
-
-  // opponentAttackMedium: function (){
-  //     var spell=spellsArray[3];
-  //     console.log(spell);
-  //     var totalDamage= deathEaterArray[0].intelligence * 2;
-  //     test.hitpoints=test.hitpoints-totalDamage;
-  //     console.log("test's hp:" + test.hitpoints);
-
-  // }, 
-
-  // opponentAttackHard: function (){
-  //     var totalDamage= deathEaterArray[0].intelligence * 3;
-  //     var spell=spellsArray[5];
-  //     console.log(spell);
-  //     test.hitpoints=test.hitpoints-totalDamage;
-  //     console.log("test's hp:" + test.hitpoints);
-  // },
-
-  // opponentAttackInstantKill: function (){
-  //     var spell=spellsArray[1];
-  //     console.log(spell);
-  //     var totalDamage= test.hitpoints
-  //     test.hitpoints=test.hitpoints-totalDamage;
-  //     console.log("test's hp:" + test.hitpoints);
-
-  // }, 
+ 
 
   
 
@@ -361,166 +407,27 @@ var Combat={
 
 
 
-var key = "$2a$10$9TJcV993TdR154smSIsF5e1Lqac..133PduyWSTXpWEG5tw.SG.1y"
-var queryURL = "https://www.potterapi.com/v1/";
-var spellsArray=[];
-var Spell = function (id, name, chtype, difficulty, effect) {
-    this.id=id;
-    this.name = name;
-    this.chtype = chtype;//player or opponent
-    this.difficulty=difficulty //how hard it is to cast spell- easy, medium, hard
-    this.effect = effect; //i.e. stun(optional), damage, heal, kill (special death eater spell)
-    //this.level = level; //only available for certain levels 
-    //function that shows spell options for character
-
-    //icon for spell
-
-};
-
-
-function getSpells(){
-    $.ajax(
-        {
-            url: queryURL + "spells" + "?key=" + key,
-            method: "GET"
-        }).then(function(response){
-            console.log("=================")
-            console.log("spells")
-            console.log("==================")
-            j=0;
-            spellsArray.push(new Spell(0, response[58].spell,"player", "hard", "damage"));
-            spellsArray.push( new Spell (1, response[15].spell, "opponent", "superHard", "instantKill"));
-            spellsArray.push( new Spell(2, response[87].spell, "opponent", "easy", "damage"));
-            spellsArray.push(new Spell(3, response[34].spell, "opponent", "medium", "damage"));
-            spellsArray.push(new Spell(4, response[59].spell, "player", "easy", "damage"));
-            spellsArray.push(new Spell(5, response[136].spell, "opponent", "hard", "damage"));
-            spellsArray.push (new Spell(6, response[51].spell, "player", "medium", "damage"));
-            spellsArray.push (new Spell(7, response[63].spell, "player", "medium", "defense"));
-            spellsArray.push (new Spell(8, response[53].spell, "player", "easyLimited", "heal"));
-
-
-            console.log(spellsArray)
-
-        });
 
 
 
 
-};
 
-var key = "$2a$10$9TJcV993TdR154smSIsF5e1Lqac..133PduyWSTXpWEG5tw.SG.1y"
-var queryURL = "https://www.potterapi.com/v1/";
-
-var deathEaterArray=[];
-getDeathEaters();
 
 
 function  getOpponent(arr) {
+
      
+  //var randomIndex = 
+  console.log("---> ",arr)
     return (arr[Math.floor(Math.random() * arr.length)]);
 };
 
 
-var DeathEater = function (id, name, intelligence, hitpoints) {
-    this.id=id
-    this.name = name;
-    //this.Spell=[];
-    //this.role = role;
-    //this.stunned = false;
-    this.intelligence = intelligence;
-    this.hitpoints = hitpoints;
-    //this.level = level;
-        //random when create character
-        //filter out opponents that are too difficult 
-    //this.experience = experience;
 
-    //function to show stats
-    //function to cast spell
-    //random spell 1, 2, 3 strength
-    //low chance of instant kill
-    this.printTest = function () {
-        console.log("Name: " + this.name);
-        console.log("\n-------------\n");
-
-        
-    };
-
- 
-    // this.attack = function (){
-    //     for(var i=0; i<spellsArray.length;i++)
-    //     if (Spell.chtype==="opponent"){
-    //         console.log(Spell[i])
-    //     }
-
-    // }
-
-
-    // this.attack ===>  if you loose then your experie decre 
-    // this.defend == > 
-
-    //function to show stats
-    //function to cast spell
-    //on-click
-    //if easy spell: 
-    //always works hit opponent intelligence * 1
-    //if medium spell:
-    //50/50 chance hit opponent intelligence * 2
-    //if hard spell:
-    //25/100 hit opponent intelligence * 3
-    //health
-        //1 time to heal
-    //if defend?? 
-        //50/50 chance
-
-
-    //if opponent hp && player hp > 0 game continues
-    //else if opponent hp < 0 player wins; ++experience
-
-    //else if player hp < 0 player loses; --experience
+  
+    
 
 
 
 
-
-};
-
-function getDeathEaters() {
-    $.ajax(
-        {
-            url: queryURL + "characters" + "?key=" + key,
-            method: "GET"
-        }).then(function (response) {
-
-
-            console.log("==========================")
-            console.log("combat characters (malevolent)")
-            //these characters are all death eaters
-            console.log("==========================")
-
-
-            for (var i = 0; i < response.length; i++) {
-                if (response[i].deathEater === true) {
-                   // deathEaterArray.push({ name: response[i].name, });
-                   // use random
-                    deathEaterArray.push(new DeathEater(i, response[i].name, 5, 100+i))
-                };
-
-                               
-            };
-            
-            console.log(deathEaterArray);
-            console.log(getOpponent(deathEaterArray))
-
-
-        });
-
-
-    };
-
-var player;
-var heal = 2;
-
-
-
-console.log(test);
 
