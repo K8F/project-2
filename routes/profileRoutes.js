e4a0726ea03fa96965836f357efb59cd1bff1395
Kate Foust <kate.e.foust@gmail.com>
2020-03-25 12:30:04 -0700

    able to get user stats to display

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -12,7 +12,14 @@ router.get("/", authCheck, (req,res)=>{
     console.log("=====================")
     console.log('HERE IS THE PROFILE ', req.app.get('user'))
     console.log("=====================")
-    res.send('you are logged in, this is your profile - ' + req.app.get("user").dataValues.name);
+    res.render('dashboard', {
+        name: req.app.get("user").dataValues.name,
+        hitpoints: req.app.get("user").dataValues.hitpoints,
+        intelligence:req.app.get("user").dataValues.intelligence 
+        //req.app.get("user").dataValues}
+    })
+        
+    //res.send('you are logged in, this is your profile - ' + req.app.get("user").dataValues.name);
 });
 
 module.exports=router;
