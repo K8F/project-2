f4c31c51b398fa110655e44eca2230cfdbd10a41
Kate Foust <kate.e.foust@gmail.com>
2020-03-24 22:33:48 -0700

    remvoed keys.js from gitignore and moved keys to a .env file

---
+++ config/keys.js
@@ -0,0 +1,17 @@
+
+
+
+
+module.exports={
+    google:{
+        clientID: process.env.google_clientID,
+        clientSecret: process.env.google_clientSecret
+    },
+
+    session:{
+        cookieKey: process.env.session
+    }
+
+};
+
+
